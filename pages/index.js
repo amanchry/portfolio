1ead3c2770c4b763b73f7e60fc252fd75a36f3ee
Aman Chaudhary <amanchaudhary.web@gmail.com>
2025-12-04 08:40:47 -0500

    updated

--- pages/index.js
+++ pages/index.js
@@ -79,7 +79,7 @@ const HomePage = () => {
                                 <div className="col-md-12 col-lg-6 order-2 order-lg-1">
                                     <h2 className="font-weight-300">Aman Chaudhary <br />
                                         <p className="font-weight-300" style={{ fontSize: "16px" }}> Geospatial Technology Developer | Remote Sensing | GeoAI <br />
-                                            World Bank | University of Twente | IIT Gandhinagar
+                                            World Bank Group | University of Twente | IIT Gandhinagar
                                         </p>
                                     </h2>
 
@@ -135,7 +135,7 @@ const HomePage = () => {
                                         <li><a href="https://medium.com/@chaudharyaman" target='_blank' rel="noreferrer" className="site-button  circle mr-1 facebook" style={{ fontSize: "24px" }}><i className="fa fa-medium"></i></a></li>
                                         <li><a href="mailto:amanchaudhary.web@gmail.com" className="site-button  circle mr-1 facebook" style={{ fontSize: "24px" }}><i className="fa fa-envelope"></i></a></li>
                                     </ul>
-                                    
+
                                 </div>
                                 <div className="col-md-12 col-lg-6 order-1 order-lg-2">
                                     <div className="img-half-bx align-items-stretch">
